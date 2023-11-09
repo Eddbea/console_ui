c43b3e3b6814a9fcb9cf3800671dd018401183fb
Edouard Leroy <leroyedouard@protonmail.com>
2023-11-09 19:15:28 +0100

    No32 Exercice3 part 1

--- src/main/java/com/eddbea/tennis/UI.java
+++ src/main/java/com/eddbea/tennis/UI.java
@@ -4,7 +4,11 @@ import com.eddbea.tennis.controller.JoueurController;
 
 public class UI {
     public static void main(String[] args) {
+
         JoueurController controller = new JoueurController();
-        controller.afficherDetailsJoueur();
+        //controller.afficherDetailsJoueur();
+        controller.creerJoueur();
+
+
     }
 }
--- src/main/java/com/eddbea/tennis/controller/JoueurController.java
+++ src/main/java/com/eddbea/tennis/controller/JoueurController.java
@@ -18,8 +18,26 @@ public class JoueurController {
         long identifiant = scanner.nextLong();
         Joueur joueur = joueurService.getJoueur(identifiant);
         System.out.println("Le joueur selectionne s'appelle "+joueur.getPrenom()+" "+joueur.getNom());
-
-
     }
+     public void creerJoueur(){
+        Scanner scanner = new Scanner(System.in);
+        Joueur joueurNouveau = new Joueur();
 
+         System.out.println("Quel est le nom du joueur que vous souhaitez creer");
+         String nomJoueur = scanner.nextLine();
+         joueurNouveau.setNom(nomJoueur);
+         System.out.println("Quel est le prenom du joueur que vous souhaitez creer");
+         String prenomJoueur = scanner.nextLine();
+         joueurNouveau.setPrenom(prenomJoueur);
+         System.out.println("Quel est le sexe du joueur que vous souhaitez creer");
+         char sexeJoueur = scanner.nextLine().charAt(0);
+         joueurNouveau.setSexe(sexeJoueur);
+         joueurService.createJoueur(joueurNouveau);
+
+
+
+
+
+
+     }
 }
