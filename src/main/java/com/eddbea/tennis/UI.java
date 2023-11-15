ab8fcc9f9c27210f611c386ec88c10034cba7857
Edouard Leroy <leroyedouard@protonmail.com>
2023-11-15 18:37:52 +0100

    No44 Exercice 5.

--- src/main/java/com/eddbea/tennis/UI.java
+++ src/main/java/com/eddbea/tennis/UI.java
@@ -6,12 +6,12 @@ import com.eddbea.tennis.controller.TournoiController;
 public class UI {
     public static void main(String[] args) {
 
-        JoueurController controller = new JoueurController();
+        //JoueurController controller = new JoueurController();
         //controller.afficherDetailsJoueur();
-        controller.creerJoueur();
-        //TournoiController tournoiController = new TournoiController();
+        //controller.creerJoueur();
+        TournoiController tournoiController = new TournoiController();
         //tournoiController.afficheDetailTournoi();
-        //tournoiController.creerTournoi();
+        tournoiController.creerTournoi();
         
     }
 }
